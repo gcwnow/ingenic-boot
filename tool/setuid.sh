021dbea144becc83a40392755b15734c1ffc7edd
Duke Fong <duke@dukelec.com>
2012-07-11 10:58:29 +0800

    add udev rule file

--- tool/setuid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export TOOLPATH=$(cd "$(dirname "$0")"; pwd)
-
-chown root $TOOLPATH/../bin/basic_cmd_tool
-chown root $TOOLPATH/../bin/stage2_cmd_tool
-chmod +s $TOOLPATH/../bin/basic_cmd_tool
-chmod +s $TOOLPATH/../bin/stage2_cmd_tool
-ln -s $TOOLPATH/../lib/libcmd.so /usr/lib/libcmd.so
